3568e5634690a74c64af4174038dcfcdc6081aff
Sameh El Zankalony <zankalony@gmail.com>
2024-08-26 15:04:35 +0300

    chore: Update About page layout
    Signed-off-by: Sameh El Zankalony <zankalony@gmail.com>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 sm:gap-2 md:gap-4">
+    <div className="flex my-0 mx-auto">
       <h1>About Page</h1>
     </div>
   );
