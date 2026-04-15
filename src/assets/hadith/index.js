7c557753b0425139de459d2a7d6fd0f10f412831
Sameh El Zankalony <zankalony@gmail.com>
2026-04-15 19:10:28 +0200

    split hadith data into 9 parts to minimize the processing time

---
+++ src/assets/hadith/index.js
@@ -0,0 +1,23 @@
+import juz1 from './juz1';
+import juz2 from './juz2';
+import juz3 from './juz3';
+import juz4 from './juz4';
+import juz5 from './juz5';
+import juz6 from './juz6';
+import juz7 from './juz7';
+import juz8 from './juz8';
+import juz9 from './juz9';
+
+const hadith = [
+  ...juz1,
+  ...juz2,
+  ...juz3,
+  ...juz4,
+  ...juz5,
+  ...juz6,
+  ...juz7,
+  ...juz8,
+  ...juz9,
+];
+
+export default hadith;
